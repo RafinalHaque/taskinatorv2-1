9546dd245c8b33cdeb832f93498d05ef6617dac8
AsproOG <116924836+AsproOG@users.noreply.github.com>
2023-06-11 13:44:46 -0400

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import homeVideo from './video.mp4';
 import "./styles.css";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -9,8 +10,20 @@ export default function Home() {
         <source src={homeVideo} type="video/mp4" />
       </video>
       <div className="centered-content">
-        <h1>Home</h1>
+        <h1>Taskinator</h1>
+        <h2> Taskinator is your task-getting solution!</h2>
+        <h2> Find tasks listed by top companies in your field and get experience!</h2>
+        <h2 class="h2-button">Get Started Now!
+        <NavLink to="/Signin" className={({ isActive }) => "nav-links" + (isActive ? " activated" : "")
+                            }
+                                
+                            >
+                                </NavLink>
+        
+        </h2>
+
       </div>
+      
     </div>
   );
 }
